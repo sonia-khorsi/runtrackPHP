fe0f567228003e1ac6c05993efcf9338cb60e801
sonia-khorsi <sonia.khorsi@laplateforme.io>
2025-01-17 14:43:34 +0100

    jour2

---
+++ jour01/job02/index.php
@@ -0,0 +1,19 @@
+
+<?php
+echo "Hello Laplateforme";
+?>
+<?php
+$str = "LaPlateforme"; // Création de la variable
+echo $str . "<br>"; // Affichage de son contenu
+?>
+<?php
+// Première ligne affichant "Hello LaPlateforme!"
+echo "Hello LaPlateforme!";
+
+/*
+   Deuxième ligne affichant "Hello LaPlateforme!"
+   Ce commentaire est au format bloc.
+*/
+echo "Hello LaPlateforme!";
+
+?>
---
+++ jour02/job01/index.php
@@ -0,0 +1,11 @@
+
+<?php
+for ($i = 0; $i <= 1337; $i++) 
+if ($i == 42){
+    echo "<strong>$i</strong><br>";
+}
+else
+{ echo "$i<br>";
+}
+
+?>
